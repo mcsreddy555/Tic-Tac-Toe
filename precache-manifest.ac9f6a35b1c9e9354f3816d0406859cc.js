0c12677b6888b68f3706abb86f6e688b8d953f39
mcsreddy555 <chandumullangi3@gmail.com>
2020-10-03 16:01:12 +0530

    Updates

---
+++ precache-manifest.ac9f6a35b1c9e9354f3816d0406859cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e8393eb3568fdcc063e7390378d040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0a5881b367a06f90930",
+    "url": "/static/css/main.02584521.chunk.css"
+  },
+  {
+    "revision": "7758f28ca871067edcd8",
+    "url": "/static/js/2.5a533665.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a533665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a5881b367a06f90930",
+    "url": "/static/js/main.2ce2ae29.chunk.js"
+  },
+  {
+    "revision": "441cfea73f775386a3f5",
+    "url": "/static/js/runtime-main.02030f09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
